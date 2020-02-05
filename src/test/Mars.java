9fe15b83beaf31f3a105db5e63c8d37998251afa
TubaBee <57161972+TubaBee@users.noreply.github.com>
2020-02-04 21:04:17 -0500

    Update Mars.java
    this is my remote changes

--- src/test/Mars.java
+++ src/test/Mars.java
@@ -1,5 +1,13 @@
 package test;
 
 public class Mars {
+    public static void main(string[] args){
+  System.out.println("Hello world");
+  
+  
+  
+  System.out.println("Hello world");
+      System.out.println("Gitten ekleyip eclipse gondericez");
 
+}
 }
